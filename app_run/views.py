f8787db120b1280e88a313740f3ac0f0504d704d
Roman Bogdanov <r.bogdanov@gmsservices.ru>
2025-07-06 21:06:07 +0300

    some refactoring

--- app_run/views.py
+++ app_run/views.py
@@ -1,15 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.conf import settings
 
 # Create your views here.
 @api_view(['GET'])
 def get_company_details(request):
     return Response(
         {
-            'company_name': 'Бегун бегуныч',
-            'slogan': 'Бегаем шустро...как пуля!',
-            'contacts': 'Город Майами, улица Дуэйн Скала Джонсон, дом 5'
+            'company_name': settings.COMPANY_NAME,
+            'slogan': settings.SLOGAN,
+            'contacts': settings.CONTACTS,
         },
         status=status.HTTP_200_OK
     )
--- project_run/settings/base.py
+++ project_run/settings/base.py
@@ -114,3 +114,7 @@ STATIC_ROOT = 'static'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+COMPANY_NAME = 'Бегун бегуныч'
+SLOGAN = 'Бегаем шустро...как пуля!'
+CONTACTS = 'Город Майами, улица Дуэйн Скала Джонсон, дом 5'
