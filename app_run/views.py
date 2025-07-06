2d1d2d82f47d00fb6643b7051b2c16a03946c8e7
Roman Bogdanov <r.bogdanov@gmsservices.ru>
2025-07-06 20:59:36 +0300

    added first endpoint 'get_company_details'

--- app_run/views.py
+++ app_run/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+@api_view(['GET'])
+def get_company_details(request):
+    return Response(
+        {
+            'company_name': 'Бегун бегуныч',
+            'slogan': 'Бегаем шустро...как пуля!',
+            'contacts': 'Город Майами, улица Дуэйн Скала Джонсон, дом 5'
+        },
+        status=status.HTTP_200_OK
+    )
