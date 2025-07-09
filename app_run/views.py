67155520ba7142c3d54e5242ec2b7800198cd708
Roman Bogdanov <r.bogdanov@gmsservices.ru>
2025-07-09 19:22:28 +0300

    added model, serializer, viewset for Run

--- app_run/views.py
+++ app_run/views.py
@@ -1,7 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 from django.conf import settings
+
+from .models import Run
+from .serializers import RunSerializer
 
 # Create your views here.
 @api_view(['GET'])
@@ -14,3 +18,7 @@ def get_company_details(request):
         },
         status=status.HTTP_200_OK
     )
+
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
