cba922cbc972fff563bf4af78d5b531c44b815e1
rcfixer <fobas505@gmail.com>
2025-10-27 19:43:15 +0300

    added runs_finished field into UserSerializer

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -17,10 +17,14 @@ class RunSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     type = serializers.SerializerMethodField()
+    runs_finished = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ['id', 'date_joined', 'username', 'last_name', 'first_name', 'type']
+        fields = ['id', 'date_joined', 'username', 'last_name', 'first_name', 'type', 'runs_finished']
 
     def get_type(self, obj):
         return 'coach' if obj.is_staff is True else 'athlete'
+
+    def get_runs_finished(self, obj):
+        return obj.runs.filter(status='finished').count()
